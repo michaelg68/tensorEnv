654e72fad6523bd2a3bcbcd03139dce74b4023d7
michaelg68 <michael.goldgamer@gmail.com>
2020-12-08 23:03:48 +0200

    Saving & Training Multiple Models - Finished https://www.techwithtim.net/tutorials/machine-learning-python/saving-models/

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
-import matplotlib as mp
+import matplotlib.pyplot as plt
 from matplotlib import style
 import pickle
 
@@ -21,36 +21,42 @@ y = np.array(data[predict]) #Label - the data we are trying to predict
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
 
-'''
-print("x_train:", x_train)
-print("x_test:", x_test)
-print("y_train:", y_train)
-print("y_test:", y_test)
-'''
+# TRAIN MODEL MULTIPLE TIMES FOR BEST SCORE
+best = 0
+for _ in range(100):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+    linear = linear_model.LinearRegression()
 
-linear = linear_model.LinearRegression()
+    #Training our model:
+    linear.fit(x_train, y_train)
 
-#Training our model:
-linear.fit(x_train, y_train)
+    #Verifying how accurate is our model:
+    acc = linear.score(x_test, y_test)
+    print("Prediction accuracy:", acc)
+    if acc > best:
+        best = acc
+        #Storing our trained model called "linear" in a file
+        with open("studentgrades.pickle", "wb") as picklefile:
+            pickle.dump(linear, picklefile)
 
-#Storing our trained model called "linear" in a file
-with open("studentgrades.pickle", "wb") as picklefile:
-    pickle.dump(linear, picklefile)
+print("The model is stored with the best accuracy:", best)
 
 
 pickle_in = open("studentgrades.pickle", "rb")
 linear = pickle.load(pickle_in)
 
-#Verifying how accurate is our model:
-acc = linear.score(x_test, y_test)
-print("Prediction accuracy:", acc)
 print("Coefficient: \n", linear.coef_)
 print("Intercept: \n", linear.intercept_)
-
 print("Checking our predictions:\n__________________")
 predictions = linear.predict(x_test) #Get a list of all predictions
 for i in range(len(predictions)):
     print(predictions[i], x_test[i], y_test[i])
 
-
+#Drawing and plotting model
+plot = "G1"
+plt.scatter(data[plot], data["G3"])
+plt.legend(loc=4)
+plt.xlabel(plot)
+plt.ylabel("Final Grade (G3)")
+plt.show()
 
